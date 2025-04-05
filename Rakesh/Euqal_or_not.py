3ffdde473d6516970ec22d3f3e2c083d87ef1639
Your Name <rakeshpatro7653@gmail.com>
2025-04-05 14:32:55 +0530

    rename

--- Rakesh/Euqal_or_not.py
+++
@@ -1,8 +0,0 @@
-# Reading two numbers from the user 
-num1 = float(input("Enter the first number: ")) 
-num2 = float(input("Enter the second number: ")) 
-# Checking if the numbers are equal 
-if num1 == num2: 
-print("The numbers are equal.") 
-else: 
-print("The numbers are not equal.")
