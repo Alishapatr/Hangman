5347630a64d3cd7799bdb4b90f39990de40b7de1
Rakesh-oracle <rakeshpatro7653@gmail.com>
2025-05-02 00:54:01 +0530

    my java files

---
+++ Rakesh/java_prog/Add_1to_n_no.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Add_1to_n_no {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter No: ");
+
+        int sum=0;
+        for(int count=1;count<10;count++)
+
+            System.out.println(sum+count);
+
+
+    }
+}
---
+++ Rakesh/java_prog/Even_or_odd.java
@@ -0,0 +1,9 @@
+public class Even_or_odd {
+    public static void main(String[] args) {
+
+       for(int i=0;i<=50;i++){
+           System.out.println("Even:"+ (2*i));
+           System.out.println("odd:"+ (2*i +1));
+       }
+    }
+}
